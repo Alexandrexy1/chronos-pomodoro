a15c7ef2eea62a47e2fe3b4cd6108372dac02f10
Alexandre Nascimento <alexandrenas2003@gmail.com>
2025-04-02 10:48:23 -0300

    container, heading and logo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
 
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
     return (
-        <div>
-            <Heading>Olá galerinha</Heading>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Molestiae, ratione animi illum deserunt, architecto quam 
-                commodi cumque officiis odit repellendus placeat at molestias 
-                corrupti minima, sunt laudantium beatae quaerat nisi.
-            </p>
-        </div>
+        <>
+            <Container>
+                <Logo/>
+            </Container>
+            <Container>
+                <section>MENU</section>
+            </Container>
+            <Container>
+                <section>PAREI</section>
+            </Container>
+            <Container>
+                <section>FOOTER</section>
+            </Container>
+        </>
     )
 }
--- src/components/Heading.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-import styles from  "./Heading.module.css";
-
-export function Heading(props: PropsWithChildren) {
-    return <h1 className={ styles.heading }>{ props.children }</h1>
-}
