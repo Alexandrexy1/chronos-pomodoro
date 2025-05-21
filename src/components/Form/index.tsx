e2c2e833d9dca972cb5464a53e9585c44be01f17
Alexandre Nascimento <alexandrenas2003@gmail.com>
2025-05-21 11:38:15 -0300

    useRef form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,12 +3,25 @@ import { Cycles } from "../Cycles";
 import { Button } from "../Button";
 
 import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { FormEvent, useRef } from "react";
 
 export function Form() {
+    const taskNameInput = useRef<HTMLInputElement>(null) as React.RefObject<HTMLInputElement>;
+
+    function handleStartNewTask(event: FormEvent<HTMLFormElement>) {
+        console.log(taskNameInput.current.value);
+        event.preventDefault();
+    }
+
     return (
-        <form action="" className="form">
+        <form onSubmit={handleStartNewTask} action="" className="form">
             <div className="formRow">
-                <Input type="text" id="myInput" placeholder="Defina um horário" />
+                <Input 
+                    type="text" 
+                    id="myInput" 
+                    placeholder="Defina um horário"
+                    ref={taskNameInput}
+                />
             </div>
             <div className="formRow">
                 <p>Lorem ipsum dolor sit amet.</p>
