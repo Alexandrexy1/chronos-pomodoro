0617d17d8c0bb9970a1c08b52afd37f8a5c613cb
Alexandre Nascimento <alexandrenas2003@gmail.com>
2025-06-08 16:08:23 -0300

    footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,13 @@
 import { RouterLink } from "../RouterLink";
+
 import styles from "./styles.module.css";
 
-
 export function Footer() {
-    return <footer className={ styles.footer }>
-        <RouterLink href="/about-pomodoro/">Entenda como funciona a técnica pomodoro</RouterLink>
-        <RouterLink href="/about-pomodoro/">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚</RouterLink>
-    </footer>
-    
+    return (
+        <footer className={ styles.footer }>
+            <RouterLink href="/about-pomodoro/">Entenda como funciona a técnica pomodoro</RouterLink>
+            <RouterLink href="/about-pomodoro/">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚</RouterLink>
+        </footer>
+    )
+ 
 }
