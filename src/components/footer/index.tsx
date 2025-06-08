40aaff2f6629342aed424b2cd353872bdb915209
Alexandre Nascimento <alexandrenas2003@gmail.com>
2025-06-08 15:40:45 -0300

    router link about-pomodoro

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 export function Footer() {
     return <footer className={ styles.footer }>
         <RouterLink href="/about-pomodoro/">Entenda como funciona a técnica pomodoro</RouterLink>
-        <RouterLink href="#">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚</RouterLink>
+        <RouterLink href="/about-pomodoro/">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚</RouterLink>
     </footer>
     
 }
